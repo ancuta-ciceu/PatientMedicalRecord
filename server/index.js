6e85e73822f320e0a87c2df23d05f99655bb65e8
Chioseaua Cristina <maria.chioseaua@student.upt.ro>
2023-03-27 19:01:26 +0300

    Initial apis for pacient table

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+//create a pacient
+app.post('/pacients', async (req, res) => {
+  try {
+    const {cnp, pacient_name, age, sex, admission_date} = req.body;
+    const newPacient = await pool.query(
+      'INSERT INTO pacient (cnp, pacient_name, age, sex, admission_date) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [cnp, pacient_name, age, sex, admission_date],
+    );
+    res.json(newPacient.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all pacients
+app.get('/pacients', async (req, res) => {
+  try {
+    const allPacients = await pool.query('SELECT * FROM pacient');
+    res.json(allPacients.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a pacient
+app.get('/pacients/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const pacient = await pool.query(
+      'SELECT * FROM pacient WHERE pacient_id = $1',
+      [id],
+    );
+    res.json(pacient.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a pacient
+app.put('/pacients/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {cnp, pacient_name, age, sex, admission_date} = req.body;
+    const updatedPacient = await pool.query(
+      'UPDATE pacient SET cnp = $1, pacient_name = $2, age = $3, sex = $4, admission_date = $5 WHERE pacient_id = $6',
+      [cnp, pacient_name, age, sex, admission_date, id],
+    );
+    res.json(updatedPacient.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+//delete a pacient
+app.delete('/pacients/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    await pool.query('DELETE FROM pacient WHERE pacient_id = $1', [id]);
+    res.json(`pacient with id ${id} was deleted`);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+app.listen(5000, () => {
+  console.log('server has started on port 5000');
+});
--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,65 +1,66 @@
 import React from 'react';
 import {Text, StyleSheet, Pressable} from 'react-native';
 
-
-const CustomButton = ({onPress, text, type = "PRIMARY", backgrColor, foregrColor}) => {
-    return (
-        
-        <Pressable 
-        onPress={onPress} 
-        style={[styles.container, styles[`container_${type}`],
+const CustomButton = ({
+  onPress,
+  text,
+  type = 'PRIMARY',
+  backgrColor,
+  foregrColor,
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        styles.container,
+        styles[`container_${type}`],
         backgrColor ? {backgroundColor: backgrColor} : {},
         styles.buttonGoolge,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+          styles[`text_${type}`],
+          foregrColor ? {color: foregrColor} : {},
         ]}>
-       
-        
-            <Text style={[
-                styles.text, 
-                styles[`text_${type}`],
-                foregrColor ? {color: foregrColor} : {}
-            ]}
-            >
-                {text}
-            </Text>
-        </Pressable>
-    );
+        {text}
+      </Text>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width:'75%',
-        padding: 15,
-        marginVertical: 5,
-        alignItems: 'center',
-        borderRadius: 20,
-        marginLeft: 50,
-    },
-    container_PRIMARY: {
-        backgroundColor: '#C4BBFF',
+  container: {
+    width: '75%',
+    padding: 15,
+    marginVertical: 5,
+    alignItems: 'center',
+    borderRadius: 20,
+    marginLeft: 50,
+  },
+  container_PRIMARY: {
+    backgroundColor: '#C4BBFF',
+  },
+  container_SECONDARY: {
+    marginVertical: 170,
+  },
+  container_TERTIARY: {
+    backgroundColor: 'transparent',
+    marginVertical: 5,
+    marginLeft: 150,
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+  text_PRIMARY: {
+    color: '#000',
+  },
+  text_SECONDARY: {
+    color: '#2E5F9B',
+  },
 
-    },
-    container_SECONDARY: {
-        marginVertical:170,
-
-    },
-    container_TERTIARY: {
-        backgroundColor: 'transparent',
-        marginVertical: 5,
-        marginLeft: 150,
-    },
-    text:{
-        fontWeight: 'bold',
-    },
-    text_PRIMARY:{
-        color: '#000',
-    },
-    text_SECONDARY: {
-        color: '#2E5F9B',
-    },
-
-    text_TERTIARY:{
-        color: '#3d85c6',
-    },
-
+  text_TERTIARY: {
+    color: '#3d85c6',
+  },
 });
 export default CustomButton;
