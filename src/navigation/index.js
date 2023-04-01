eb537dcb72823ce3dadc0ce1cb98b2348e5a9fc3
chios <maria.chioseaua@student.upt.ro>
2023-04-01 20:44:33 +0300

    Initial scan of qr code

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInAsDoctorScreen from '../screens/SignInAsDoctorScreen';
 import SignInAsMedicalAssistantScreen from '../screens/SignInAsMedicalAssistantScreen';
 import SignInAsScreen from '../screens/SignInAsScreen';
+import {QRCodeScannerScreen} from '../screens/QRCodeScreens/ScanQRCode';
+import {PacientFormForDoctorScreen} from '../screens/PacientFormScreens/PacientFormForDoctor';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="SignInAsScreen" component={SignInAsScreen} />
-                <Stack.Screen name="SignInAsDoctorScreen" component={SignInAsDoctorScreen} />
-                <Stack.Screen name="SignInAsMedicalAssistantScreen" component={SignInAsMedicalAssistantScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="SignInAsScreen" component={SignInAsScreen} />
+        <Stack.Screen
+          name="SignInAsDoctorScreen"
+          component={SignInAsDoctorScreen}
+        />
+        <Stack.Screen
+          name="SignInAsMedicalAssistantScreen"
+          component={SignInAsMedicalAssistantScreen}
+        />
+        <Stack.Screen
+          name="QRCodeScannerScreen"
+          component={QRCodeScannerScreen}
+        />
+        <Stack.Screen
+          name="PacientFormForDoctorScreen"
+          component={PacientFormForDoctorScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
