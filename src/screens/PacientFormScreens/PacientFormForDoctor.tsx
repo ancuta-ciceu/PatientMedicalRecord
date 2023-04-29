fdf85bd62f82bf03bc84338d78350d9d7174d5e4
chios <maria.chioseaua@student.upt.ro>
2023-04-29 18:09:39 +0300

    advanced with doctor form

--- src/screens/PacientFormScreens/PacientFormForDoctor.tsx
+++ src/screens/PacientFormScreens/PacientFormForDoctor.tsx
@@ -62,113 +62,103 @@ export const PacientFormForDoctorScreen = () => {
     console.log(patient);
   }, [patient]);
 
-  const {register, handleSubmit} = useForm();
+  const {register, getValues} = useForm();
 
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  //
-  // const handleSubmit = () => {
-  //   console.log('Submitted!');
-  //   // Add your form submission logic here
-  // };
-  //
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text style={styles.title}>Sign Up</Text>
-  //       <TextInput
-  //         style={styles.input}
-  //         placeholder="Name"
-  //         value={name}
-  //         onChangeText={setName}
-  //       />
-  //       <TextInput
-  //         style={styles.input}
-  //         placeholder="Email"
-  //         value={email}
-  //         onChangeText={setEmail}
-  //         keyboardType="email-address"
-  //         autoCapitalize="none"
-  //       />
-  //       <TextInput
-  //         style={styles.input}
-  //         placeholder="Password"
-  //         value={password}
-  //         onChangeText={setPassword}
-  //         secureTextEntry
-  //         autoCapitalize="none"
-  //       />
-  //       <TouchableOpacity style={styles.button}>
-  //         <Text style={styles.buttonText}>Submit</Text>
-  //       </TouchableOpacity>
-  //     </View>
-  //   );
-  // };
+  const handleSubmit = () => {
+    console.log(getValues());
+    // Add your form submission logic here
+  };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 20,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-    },
-    input: {
-      width: '100%',
-      height: 50,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 5,
-      paddingHorizontal: 10,
-      marginBottom: 10,
-    },
-    button: {
-      backgroundColor: 'blue',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  });
-
+  const [open, setOpen] = useState<boolean>(false);
+  const [date, setDate] = useState(new Date());
   return (
-    //<form onSubmit={handleSubmit(dataa => console.log(dataa))}>
-    <View>
-      <Text> Pacient Form </Text>
-      <Text>{patient.patientName}</Text>
-      <View>
-        <Text>Medicine name:</Text>
-        <TextInput {...register('medicine')} />
-        <Text>Type of administration:</Text>
-        <TextInput
-          {...register('administrationType')}
-          // placeholder="Type of administration"
-        />
-        <Text>Number of days to administrate::</Text>
-        <TextInput
-          {...register('days')}
-          // placeholder="Number of days to administrate:"
-          //type={'number'}
-        />
-        <Text>Number of times/day to administrate::</Text>
-        <TextInput
-          {...register('timesPerDay')}
-          // placeholder="Number of times/day to administrate:"
-          //type={'number'}
-        />
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Patient Form</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        {...register('patientName')}
+      />
+      <TextInput style={styles.input} placeholder="CNP" {...register('CNP')} />
+      <TextInput
+        style={styles.input}
+        placeholder="Age"
+        {...register('age')}
+        keyboardType={'numeric'}
+      />
+      <TextInput style={styles.input} placeholder="Sex" {...register('sex')} />
+      <Button title="Admission date" onPress={() => setOpen(true)} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
-    //</form>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+//
+//   return (
+//     //<form onSubmit={handleSubmit(dataa => console.log(dataa))}>
+//     <View>
+//       <Text> Pacient Form </Text>
+//       <Text>{patient.patientName}</Text>
+//       <View>
+//         <Text>Medicine name:</Text>
+//         <TextInput {...register('medicine')} />
+//         <Text>Type of administration:</Text>
+//         <TextInput
+//           {...register('administrationType')}
+//           // placeholder="Type of administration"
+//         />
+//         <Text>Number of days to administrate::</Text>
+//         <TextInput
+//           {...register('days')}
+//           // placeholder="Number of days to administrate:"
+//           //type={'number'}
+//         />
+//         <Text>Number of times/day to administrate::</Text>
+//         <TextInput
+//           {...register('timesPerDay')}
+//           // placeholder="Number of times/day to administrate:"
+//           //type={'number'}
+//         />
+//       </View>
+//     </View>
+//     //</form>
+//   );
+// };
 
 // const styles = StyleSheet.create({
 //   //Check project repo for styles
