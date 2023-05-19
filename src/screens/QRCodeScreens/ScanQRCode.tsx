d8217910a724515969b8c5de4e6b09c0577e3687
chios <maria.chioseaua@student.upt.ro>
2023-05-19 19:55:05 +0300

    Made qr work and added navigation

--- src/screens/QRCodeScreens/ScanQRCode.tsx
+++ src/screens/QRCodeScreens/ScanQRCode.tsx
@@ -1,79 +1,73 @@
 'use strict';
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import {useNavigation} from '@react-navigation/native';
 
 export const QRCodeScannerScreen = () => {
-  const [data, setData] = useState<String>('Set data');
+  const [data, setData] = useState<String>('');
+  const [scanning, setScanning] = useState<boolean>(true);
   const navigation = useNavigation();
-  const onSignInPressed = () => {
-    // @ts-ignore
-    navigation.navigate('SignInAsScreen');
+  useEffect(() => {
+    if (!scanning && data !== '') {
+      // @ts-ignore
+      navigation.navigate('PacientFormForDoctorScreen', {id: data});
+    }
+  }, [scanning, data, navigation]);
+
+  const handleQRRead = ({data}: {data: string}) => {
+    // Check if the scanned QR code is valid
+
+    setScanning(false); // Stop scanning
+    setData(data);
+    console.log(data);
   };
+
+  // const isValidQRCode = (data: string) => {
+  //   return data.includes('your-validation-keyword');
+  // };
 
   return (
     <QRCodeScanner
-      onRead={({data}) => {
-        setData(data);
-        console.warn(data);
-      }}
-      reactivate={true}
-      //cameraTimeout={5000}
-      topContent={
-        <View>
-          <Text>{data}</Text>
-        </View>
-      }
+      onRead={handleQRRead}
+      reactivate={scanning}
+      cameraTimeout={5000}
       showMarker={true}
-      bottomContent={
-        <>
-          <View>
-            <Text>QR Code Scanner</Text>
-          </View>
-          <CustomButton
-            text="Sign In"
-            onPress={onSignInPressed}
-            backgrColor={undefined}
-            foregrColor={undefined}
-          />
-        </>
+      topContent={
+        <Text style={styles.centerText}>
+          <Text style={styles.textBold}>Please scan QR</Text>
+        </Text>
       }
-
-      // topContent={
-      //   <Text style={styles.centerText}>
-      //     Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text>{' '}
-      //     on your computer and scan the QR code.
-      //   </Text>
-      // }
-      // bottomContent={
-      //   <TouchableOpacity style={styles.buttonTouchable}>
-      //     <Text style={styles.buttonText}>OK. Got it!</Text>
-      //   </TouchableOpacity>
-      // }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>
+            Don't want to scan? Enter it manually.
+          </Text>
+        </TouchableOpacity>
+      }
     />
   );
 };
 
-// const styles = StyleSheet.create({
-//   centerText: {
-//     flex: 1,
-//     fontSize: 18,
-//     padding: 32,
-//     color: '#777',
-//   },
-//   textBold: {
-//     fontWeight: '500',
-//     color: '#000',
-//   },
-//   buttonText: {
-//     fontSize: 21,
-//     color: 'rgb(0,122,255)',
-//   },
-//   buttonTouchable: {
-//     padding: 16,
-//   },
-// });
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
